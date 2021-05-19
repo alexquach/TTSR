78be1263d767397c89aed5cc8dcd68e29e50211d
Ishana Shastri <ishana@mit.edu>
2021-05-19 11:39:45 -0400

    added test set compatibility

--- dataset/dataloader.py
+++ dataset/dataloader.py
@@ -18,7 +18,7 @@ def get_dataloader(args):
             data_train, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
         dataloader_test = {}
         for i in range(5):
-            data_test = getattr(m, 'TrainSet')(args=args)
+            data_test = getattr(m, 'TestSet')(args=args)
             dataloader_test[str(i+1)] = DataLoader(data_test, batch_size=1,
                                                    shuffle=False, num_workers=args.num_workers)
         dataloader = {'train': dataloader_train, 'test': dataloader_test}
@@ -35,7 +35,6 @@ def get_dataloader(args):
             dataloader_test[str(i+1)] = DataLoader(data_test, batch_size=1,
                                                    shuffle=False, num_workers=args.num_workers)
         dataloader = {'train': dataloader_train, 'test': dataloader_test}
-
 
     else:
         raise SystemExit('Error: no such type of dataset!')
